6d1124933c5af358c119ad039cedc8472ce5a257
Abhiek187 <achaudhuri2011@yahoo.com>
2020-05-29 21:48:23 -0400

    Use Rectangle class to determine bounds

--- src/game.ts
+++ src/game.ts
@@ -68,26 +68,26 @@ export default class Tetris extends Phaser.Scene {
 
 	update(time: number, delta: number) {
 		// Check properties per frame
-		const sideDelta: number = Constants.blockData[this.currentBlockType].sideDelta;
-		const bottomDelta: number = Constants.blockData[this.currentBlockType].bottomDelta;
+		const bounds: Phaser.Geom.Rectangle = this.currentBlock.getBounds();
 
-		if (this.cursors.left.isDown && !this.holdingLeft
-			&& this.currentBlock.x - sideDelta > 25) {
+		// Only register pressing down button, not holding it down
+		if (this.cursors.left.isDown && !this.holdingLeft && bounds.left > 25) {
 			this.currentBlock.x -= 25;
 			this.holdingLeft = true;
-		} else if (this.cursors.right.isDown && !this.holdingRight
-			&& this.currentBlock.x + sideDelta < 275) {
+		} else if (this.cursors.right.isDown && !this.holdingRight && bounds.right < 275) {
 			this.currentBlock.x += 25;
 			this.holdingRight = true;
 		} else if (this.cursors.up.isDown && !this.holdingUp) {
-			this.currentBlock.y = 525 - bottomDelta;
+			// Hard drop (go to the bottom immediately)
+			this.currentBlock.y = 525 - bounds.height / 2;
 			this.holdingUp = true;
 		} else if (this.cursors.down.isDown && !this.holdingDown) {
-			this.fallDelta /= 8;
+			// Soft drop (speed up)
+			this.fallDelta /= 16;
 			this.holdingDown = true;
 		} else if (this.cursors.space.isDown && !this.holdingSpace) {
 			// 0: (x, y), 90: (-y, x), 180: (-x, -y), 270: (y, -x)
-			this.currentBlock.angle += 90;
+			this.rotateBlock();
 			this.holdingSpace = true;
 		}
 
@@ -101,7 +101,7 @@ export default class Tetris extends Phaser.Scene {
 			this.holdingUp = false;
 		}
 		if (this.cursors.down.isUp && this.holdingDown) {
-			this.fallDelta *= 8;
+			this.fallDelta *= 16;
 			this.holdingDown = false;
 		}
 		if (this.cursors.space.isUp) {
@@ -109,7 +109,7 @@ export default class Tetris extends Phaser.Scene {
 		}
 
 		// Check fall timer
-		if (this.currentBlock.y + bottomDelta >= 525) {
+		if (bounds.bottom >= 525) {
 			this.createNewBlock(this.shapes);
 			this.fallTimer = 0;
 		} else if (this.fallTimer >= this.fallDelta) {
@@ -132,13 +132,42 @@ export default class Tetris extends Phaser.Scene {
 			this.currentBlockType = 'normal';
 		}
 
-		/*this.currentBlock = this.blocks.create(startX, startY, this.currentBlockStr);
-		this.currentBlock.setCollideWorldBounds(true);*/
 		const startX: number = Constants.blockData[this.currentBlockType].startX;
 		const startY: number = Constants.blockData[this.currentBlockType].startY;
 		this.currentBlock = this.matter.add.image(
 			startX, startY, this.currentBlockStr, null, shapes[this.currentBlockStr]
 		);
+	}
+
+	rotateBlock() {
+		// Adjust position of tetris block to align with the grid
+		this.currentBlock.angle += 90;
+		if (this.currentBlockStr === 'block5') return;
+
+		if (this.currentBlock.angle % 180 === 0) {
+			// Horizontal
+			this.currentBlock.x -= 12.5;
+			this.currentBlock.y -= 12.5;
+		} else {
+			// Vertical
+			this.currentBlock.x += 12.5;
+			this.currentBlock.y += 12.5;
+		}
+
+		// Move block again if it's out of bounds
+		const bounds: Phaser.Geom.Rectangle = this.currentBlock.getBounds();
+
+		if (bounds.left < 0) {
+			// Happens when the vertical block is rotated from the far left
+			this.currentBlock.x += 50;
+		} else if (bounds.left < 25) {
+			this.currentBlock.x += 25;
+		} else if (bounds.right > 275) {
+			this.currentBlock.x -= 25;
+		}
+		if (bounds.bottom > 525) {
+			this.currentBlock.y -= 25;
+		}
 	}
 }
 
